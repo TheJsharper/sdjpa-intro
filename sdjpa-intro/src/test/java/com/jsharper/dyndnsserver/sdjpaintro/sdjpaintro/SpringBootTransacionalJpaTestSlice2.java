068be56969efecd116620ffc9897f19ec1860154
Pedro Antonio Salomon Ocampos <pedro.salomon@gmail.com>
2022-07-04 16:57:59 +0200

    testing

--- sdjpa-intro/src/test/java/com/jsharper/dyndnsserver/sdjpaintro/sdjpaintro/SpringBootTransacionalJpaTestSlice2.java
+++ sdjpa-intro/src/test/java/com/jsharper/dyndnsserver/sdjpaintro/sdjpaintro/SpringBootTransacionalJpaTestSlice2.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.annotation.Order;
 import org.springframework.test.annotation.Commit;
 
@@ -15,18 +16,19 @@ import com.jsharper.dyndnsserver.sdjpaintro.sdjpaintro.repositories.BookReposito
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @DataJpaTest
+@ComponentScan(basePackages = { "com.jsharper.dyndnsserver.sdjpaintro.sdjpaintro.bootstrap" })
 public class SpringBootTransacionalJpaTestSlice2 {
 
 	@Autowired
 	private BookRepository bookRepository;
 
-	//@Rollback(value = false) that will work but by using commit better
+	// @Rollback(value = false) that will work but by using commit better
 	@Commit
 	@Order(1)
 	@Test
 	void testJpaTestSplice() {
 		long countBefore = bookRepository.count();
-
+		assertThat(countBefore).isEqualTo(2);
 		bookRepository.save(new Book("My Book ", "1245588", "Self-test"));
 
 		long countAfter = bookRepository.count();
@@ -39,8 +41,8 @@ public class SpringBootTransacionalJpaTestSlice2 {
 	@Test
 	void testJpaTestSpliceTransaction() {
 		long countBefore = bookRepository.count();
-
-		assertThat(countBefore).isEqualTo(1);
+		// because we have included bootstrap initializing packages for execution
+		assertThat(countBefore).isEqualTo(3);
 
 	}
 }
